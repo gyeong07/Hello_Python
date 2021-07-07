2038a70acfbceab6dea2f8b87b9de27fee01484e
gyeong07 <vlem4646@gmail.com>
2021-07-07 14:08:38 +0900

    Random

---
+++ practice.py
@@ -0,0 +1,27 @@
+# 숫자 자료형
+# print(5)
+# print(-10)
+# print(3.14)
+# print(5+3)
+# print(2*8)
+
+# 문자열 자료형
+# print('풍선')
+# print("나비")
+# print("z"*9)
+
+# boolean 자료형
+# print(5>10)
+# print(5<10)
+# print(not (5>10))
+
+# 변수
+# 애완동물을 소개해 주세요~
+animal = "강아지"
+name = "연탄이"
+age = 4
+hobby = "산책"
+is_adult = age >= 3
+print("우리집 " + animal + "의 이름은 " + name + "에요")
+print(name + "는 " + str(age) + "살이며, " + hobby + "을 아주 조아해요")
+print(name + "는 어른일까요? " + str(is_adult))
